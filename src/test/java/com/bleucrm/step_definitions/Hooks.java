8d84c0b2549cab22fb1915d2ac92af7c0579fc95
Berkan Acar <berkanacar@gmail.com>
2022-03-07 09:17:29 +0100

    Framework creation for Bleucrm web app

---
+++ src/test/java/com/bleucrm/step_definitions/Hooks.java
@@ -0,0 +1,31 @@
+package com.bleucrm.step_definitions;
+
+import com.bleucrm.utilities.ConfigurationReader;
+import com.bleucrm.utilities.Driver;
+import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+        Driver.get().get(ConfigurationReader.get("url"));
+        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Driver.get().manage().window().maximize();
+    }
+    @After
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        Driver.closeDriver();
+    }
+}
