3352876b5378b30c40a79529be0d1278070a0e43
Berkan Acar <berkanacar@gmail.com>
2022-03-07 22:47:44 +0100

    Abstract Basepage added to the Framework for Bleucrm web app

---
+++ src/test/java/com/bleucrm/pages/DashBoardPage.java
@@ -0,0 +1,13 @@
+package com.bleucrm.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DashBoardPage extends BasePage{
+    @FindBy(id = "feed-add-post-form-tab-vote")
+    public WebElement poll_tab;
+
+    @FindBy (id = "user-name")
+    public WebElement usernameText;
+
+}
---
+++ src/test/resources/features/poll.feature
@@ -0,0 +1,7 @@
+Feature:  Create a poll by clicking on Poll tab under Active Stream
+  Background:
+    When the user logins with a valid username and password
+    Then the user should be able to reach the dashboard page: "https://qa.bleucrm.com/stream/?login=yes"
+
+
+  Scenario:
