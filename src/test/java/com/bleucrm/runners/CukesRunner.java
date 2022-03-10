9ea6931dfac8b330a67eff763c2ae8fceb84fe57
Samrasky <vip.semra@gmail.com>
2022-03-10 20:20:59 +0100

    Cucumber report folder renewed

--- src/test/java/com/bleucrm/runners/CukesRunner.java
+++ src/test/java/com/bleucrm/runners/CukesRunner.java
@@ -6,8 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"json:target/cucumber/json",
-                "html:target/default-html-report",
+
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports",
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "com/bleucrm/step_definitions",
